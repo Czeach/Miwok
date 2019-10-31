b46fdd1281188e7df6dbd70e1b85a76c8a2a5f32
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2019-10-31 06:53:59 +0000

    Create Phrase.java

---
+++ app/src/main/java/com/example/android/miwok/Phrase.java
@@ -0,0 +1,66 @@
+package com.example.android.miwok;
+
+// it contains a Miwok translation and a default translation for each word
+
+public class Phrase {
+
+
+    // default translation for the word
+    private String mDefaultTranslation;
+
+    // miwok translation for the word
+    private  String mMiwokTranslation;
+
+    //private integer variable to hold the resource id
+    private int mImageResourceID = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    //private integer variable to hold the audio resource id
+    private int mAudioResourceID;
+
+    // create a new word object
+    // defaultTranslation is the word in english
+    // miwokTranslation is the word in miwok
+    public Phrase (String defaultTranslation, String miwokTranslation, int audioResourceID){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceID = audioResourceID;
+    }
+
+    // create a new word object
+    // defaultTranslation is the word in english
+    // miwokTranslation is the word in miwok
+    // imageResourceID is the image associated with the word
+    public Phrase (String defaultTranslation, String miwokTranslation, int imageResourceID, int audioResourceID){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceID = imageResourceID;
+        mAudioResourceID = audioResourceID;
+    }
+
+    // get the default translation of the word
+    public String getDefaultTranslation(){
+        return mDefaultTranslation;
+    }
+
+    // get the miwok translation of the word
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    // get the image
+    public int getImageResourceID(){
+        return mImageResourceID;
+    }
+
+    //return whether or not there is an image for this word
+    public boolean hasImage(){
+        return mImageResourceID != NO_IMAGE_PROVIDED;
+    }
+
+    // get the audio
+    public int getAudioResourceID(){
+        return mAudioResourceID;
+    }
+}
